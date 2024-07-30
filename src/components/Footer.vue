51cca3d32c43a765f4444893f218cb2326993afa
Luca <lucagcuello@gmail.com>
2024-07-30 16:43:46 -0300

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>Jemersoft - Prueba técnica</p>
+    <p>07/2024</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+footer p {
+  font-weight: 500;
+}
+</style>
